cc88332f47e665a3471cd5b9094d3016df07417c
leticianb <leticianakahara10@gmail.com>
2024-04-23 01:48:27 -0300

    Update com anotações

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
 ﻿using fundamentos_dotnet.Models;
 
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+Console.WriteLine($"Quantidade em esqtoque: {quantidadeEmEstoque}");
 
-string apresentacao = "olá, seja bem vindo";
-int quantidade = 1;
-double altura = 1.84;
-decimal preco = 1.80M;
-bool condicao = true;
-Console.WriteLine(apresentacao);
-Console.WriteLine("Quantidade de alunos: " + quantidade);
-Console.WriteLine("Altura do aluno: " + altura.ToString("0.000"));
-Console.WriteLine("Valor da variavel preço: " + preco);
-Console.WriteLine("Valor da variavel condição: " + condicao);
+if(quantidadeEmEstoque>= quantidadeCompra){
+    Console.WriteLine("Venda realizada");
+    quantidadeEmEstoque--;
+    Console.WriteLine(quantidadeEmEstoque);
+}
+else{
+    Console.WriteLine("Não foi possível efetuar a compra");
+}
+
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/mm/yyyy HH:mm"));
+
+// int a = Convert.ToInt32("5");
+// int a = int.Parse("5"); outra forma
+// Console.WriteLine(a);
+// int b = 6;
+// string c = b.ToString();
 
 
 
-
+// string apresentacao = "olá, seja bem vindo";
+// int quantidade = 1;
+// double altura = 1.84;
+// decimal preco = 1.80M;
+// bool condicao = true;
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Quantidade de alunos: " + quantidade);
+// Console.WriteLine("Altura do aluno: " + altura.ToString("0.000"));
+// Console.WriteLine("Valor da variavel preço: " + preco);
+// Console.WriteLine("Valor da variavel condição: " + condicao);
 
 
 
--- obj/Debug/net8.0/fundamentos-dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/fundamentos-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fundamentos-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4a5b56821aebfd8f73d535ea43bbd2c96ad837")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5327a16c5a0ce5fad9ae930965464550d84ab282")]
 [assembly: System.Reflection.AssemblyProductAttribute("fundamentos-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fundamentos-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
